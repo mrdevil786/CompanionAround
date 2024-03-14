e726e47792e79d0c871c6abe56022cbc276629f0
mrdevil786 <suhailsaeedme@gmail.com>
2024-03-15 00:41:38 +0530

    Abu Bhai

--- resources/views/site/layout/header.blade.php
+++ resources/views/site/layout/header.blade.php
@@ -21,9 +21,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="{{asset('site/lib/animate/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('site/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{asset('site/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')}}" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{asset('site/css/bootstrap.min.css')}}" rel="stylesheet">
