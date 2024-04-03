33fb1f22827bae68d272020fdedaa06b26f84cea
mrdevil786 <suhailsaeedme@gmail.com>
2024-04-04 03:38:24 +0530

    Dynamic Tour Guide page

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -173,22 +173,47 @@
                         <!-- LOGO -->
                     </div>
                     <div class="main-sidemenu">
+
                         <div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg"
                                 fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
                                 <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z" />
                             </svg></div>
+
                         <ul class="side-menu">
+
                             <li class="sub-category">
                                 <h3>Main</h3>
                             </li>
+                            
                             <li class="slide">
-                                <a class="side-menu__item has-link" data-bs-toggle="slide"
-                                    href="{{ url('admin/') }}"><i class="side-menu__icon fe fe-home"></i><span
+                                <a class="side-menu__item has-link {{ Request::is('admin') ? 'active' : '' }}"
+                                    data-bs-toggle="slide" href="{{ url('admin/') }}"><i
+                                        class="side-menu__icon fe fe-home"></i><span
                                         class="side-menu__label">Dashboard</span></a>
                             </li>
+
                             <li class="sub-category">
-                                <h3>UI Kit</h3>
+                                <h3>WEBSITE PANEL</h3>
                             </li>
+
+                            <li class="slide">
+                                <a class="side-menu__item has-link {{ Request::is('admin/tourists*') ? 'active' : '' }}"
+                                    data-bs-toggle="slide" href="{{ url('admin/tourists') }}"><i
+                                        class="side-menu__icon fe fe-home"></i><span
+                                        class="side-menu__label">Tourists</span></a>
+                            </li>
+
+                            <li class="slide">
+                                <a class="side-menu__item has-link {{ Request::is('admin/tourguides*') ? 'active' : '' }}"
+                                    data-bs-toggle="slide" href="{{ url('admin/tourguides') }}"><i
+                                        class="side-menu__icon fe fe-home"></i><span class="side-menu__label">Tour
+                                        Guides</span></a>
+                            </li>
+
+                            <li class="sub-category">
+                                <h3>ADMIN PANEL</h3>
+                            </li>
+
                             <li class="slide {{ Request::is('admin/users*') ? 'is-expanded' : '' }}"">
                                 <a class="side-menu__item {{ Request::is('admin/users*') ? 'active' : '' }}"
                                     data-bs-toggle="slide" href="javascript:void(0)"><i
@@ -196,21 +221,12 @@
                                         class="side-menu__label">Users</span><i
                                         class="angle fe fe-chevron-right"></i></a>
                                 <ul class="slide-menu">
-                                    <li class="side-menu-label1"><a href="javascript:void(0)">Apps</a></li>
                                     <li><a href="{{ url('admin/users/') }}"
                                             class="slide-item {{ Request::is('admin/users*') ? 'active' : '' }}">
                                             Manage Users</a></li>
-                                    <li><a href="{{ url('admin/tourists/') }}"
-                                            class="slide-item {{ Request::is('admin/tourists*') ? 'active' : '' }}">
-                                            Manage Tourists</a></li>
                                 </ul>
                             </li>
-                            <li class="slide">
-                                <a class="side-menu__item has-link" data-bs-toggle="slide"
-                                    href="{{ url('admin/tourguides') }}"><i
-                                        class="side-menu__icon fe fe-home"></i><span class="side-menu__label">Tour
-                                        Guides</span></a>
-                            </li>
+
                         </ul>
                         <div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg"
                                 fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
