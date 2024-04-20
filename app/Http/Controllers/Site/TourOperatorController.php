f4962e4a73de55d77279d687b47161eed9502566
mrdevil786 <suhailsaeedme@gmail.com>
2024-04-20 16:49:08 +0530

    Updated Tour-Operator Controller

--- app/Http/Controllers/Site/TourOperatorController.php
+++ app/Http/Controllers/Site/TourOperatorController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Site;
 
-use App\Http\Controllers\Controller;
+use App\Models\TourOperator;
 use Illuminate\Http\Request;
+use App\Helpers\FileUploader;
+use App\Http\Controllers\Controller;
 
 class TourOperatorController extends Controller
 {
@@ -11,5 +13,46 @@ class TourOperatorController extends Controller
     {
         $user = auth('touroperator')->user();
         return view('site.tourOperatorProfile', compact('user'));
+    }
+
+    public function edit(Request $request)
+    {
+        $user = TourOperator::findOrFail($request->id);
+        return $user;
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'mobile' => 'required|digits:10',
+            'address' => 'required|string',
+            'zipcode' => 'required|numeric',
+            'country' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'description' => 'required',
+            'logo' => 'required|mimes:png,jpg,jpeg,webp,svg,gif'
+        ]);
+
+        $user = TourOperator::findOrFail(auth('tourguard')->user()->id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->mobile = $request->mobile;
+        $user->address = $request->address;
+        $user->zipcode = $request->zipcode;
+        $user->country = $request->country;
+        $user->state = $request->state;
+        $user->city = $request->city;
+        $user->description = $request->description;
+
+        if ($request->hasFile('logo')) {
+            $user->logo = FileUploader::uploadFile($request->file('logo'), 'storage/images/tour-guides', $user->logo);
+        }
+
+        $user->status = 'active';
+        $user->save();
+        return redirect()->back()->with('success', 'Profile detail updated successfully!');
     }
 }
