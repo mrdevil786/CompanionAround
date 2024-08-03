7d9b3d7ebe58e1982a4de39975b1ad07e446334a
mrdevil786 <suhailsaeedme@gmail.com>
2024-08-03 11:45:32 +0530

    Login Page

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -16,8 +16,8 @@
             </div>
         </div>
     </div>
-    </div>
     <!-- Navbar & Hero End -->
+    
     <!-- Contact Start -->
     <div class="container-xxl py-5">
         <div class="container">
@@ -33,43 +33,44 @@
 
                     <form>
 
-                            <div class="col-12 mt-5">
-                                <a href="{{ URL::TO('googleLogin') }}" class="btn btn-primary w-100 py-3" type="submit"><i
-                                        class="fab fa-google me-3"></i> Continue with Google</a>
-                            </div>
+                        <div class="col-12 mt-5">
+                            <a href="{{ URL::TO('googleLogin') }}" class="btn btn-primary w-100 py-3" type="submit"><i
+                                    class="fab fa-google me-3"></i> Continue with Google</a>
+                        </div>
 
-                            <div class="col-12 mt-3">
-                                <a href="" class="btn btn-primary w-100 py-3" type="submit"><i
-                                        class="fab fa-facebook me-3"></i> Continue with
-                                    Facebook</a>
-                            </div>
+                        <div class="col-12 mt-3">
+                            <a href="" class="btn btn-primary w-100 py-3" type="submit"><i
+                                    class="fab fa-facebook me-3"></i> Continue with
+                                Facebook</a>
+                        </div>
 
                     </form>
 
                 </div>
-            <div class="row g-4 justify-content-center mt-5">
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <ul class="nav nav-pills mb-5 justify-content-center" id="pills-tab" role="tablist">
+                <div class="row g-4 justify-content-center mt-5">
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <ul class="nav nav-pills mb-5 justify-content-center" id="pills-tab" role="tablist">
 
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link rounded-1 active me-3" id="pills-home-tab" data-bs-toggle="pill"
-                                data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
-                                aria-selected="true" style="border: 1px solid #86B817;">Login</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link rounded-1" id="pills-profile-tab" data-bs-toggle="pill"
-                                data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
-                                aria-selected="false" style="border: 1px solid #86B817;">Signup</button>
-                        </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link rounded-1 active me-3" id="pills-home-tab" data-bs-toggle="pill"
+                                    data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                                    aria-selected="true" style="border: 1px solid #86B817;">Login</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link rounded-1" id="pills-profile-tab" data-bs-toggle="pill"
+                                    data-bs-target="#pills-profile" type="button" role="tab"
+                                    aria-controls="pills-profile" aria-selected="false"
+                                    style="border: 1px solid #86B817;">Signup</button>
+                            </li>
 
-                    </ul>
-                    <div class="tab-content" id="pills-tabContent">
+                        </ul>
+                        <div class="tab-content" id="pills-tabContent">
 
-                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                            aria-labelledby="pills-home-tab">
-                            <form id="post-login">
-                                <div class="row g-3" style="padding-bottom:20px">
-                                    {{-- <div class="col-md-12">
+                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                                aria-labelledby="pills-home-tab">
+                                <form id="post-login">
+                                    <div class="row g-3" style="padding-bottom:20px">
+                                        {{-- <div class="col-md-12">
                                         <div class="form-check form-check-inline">
                                             <input class="form-check-input" type="radio" name="type" id="inlineRadio2"
                                                 value="tour_guide">
@@ -81,32 +82,33 @@
                                             <label class="form-check-label" for="inlineRadio1">Tour Operator</label>
                                         </div>
                                     </div> --}}
-                                    <div class="col-12">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" name="email" id="email"
-                                                placeholder="Your Email">
-                                            <label for="email">Your Email</label>
+                                        <div class="col-12">
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" name="email" id="email"
+                                                    placeholder="Your Email">
+                                                <label for="email">Your Email</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-floating">
+                                                <input type="password" class="form-control" name="password" id="password"
+                                                    placeholder="Enter the password">
+                                                <label for="password">Password</label>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-12">
-                                        <div class="form-floating">
-                                            <input type="password" class="form-control" name="password" id="password"
-                                                placeholder="Enter the password">
-                                            <label for="password">Password</label>
-                                        </div>
+                                        <button class="btn btn-primary w-100 py-3" type="submit">Login</button>
                                     </div>
-                                </div>
-                                <div class="col-12">
-                                    <button class="btn btn-primary w-100 py-3" type="submit">Login</button>
-                                </div>
-                                <!--</div>-->
-                            </form>
-                        </div>
+                                    <!--</div>-->
+                                </form>
+                            </div>
 
-                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                            <form id="sign-up">
-                                <div class="row g-3">
-                                    {{-- <div class="col-md-12">
+                            <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                                aria-labelledby="pills-profile-tab">
+                                <form id="sign-up">
+                                    <div class="row g-3">
+                                        {{-- <div class="col-md-12">
                                         <div class="form-check form-check-inline">
                                             <input class="form-check-input" type="radio" name="type" id="inlineRadio2"
                                                 value="tour_guide">
@@ -118,41 +120,42 @@
                                             <label class="form-check-label" for="inlineRadio1">Tour Operator</label>
                                         </div>
                                     </div> --}}
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="name" name="name"
-                                                placeholder="Your Name">
-                                            <label for="name">Your Name</label>
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" id="name" name="name"
+                                                    placeholder="Your Name">
+                                                <label for="name">Your Name</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input type="email" class="form-control" id="email" name="email"
+                                                    placeholder="Your Email">
+                                                <label for="email">Your Email</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" id="mobile" name="mobile"
+                                                    placeholder="Mobile">
+                                                <label for="mobile">Mobile</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-floating">
+                                                <input type="password" class="form-control" placeholder="Password"
+                                                    name="password" id="message"></input>
+                                                <label for="message">Password</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100 py-3" type="submit">Sign Up</button>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input type="email" class="form-control" id="email" name="email"
-                                                placeholder="Your Email">
-                                            <label for="email">Your Email</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="mobile" name="mobile"
-                                                placeholder="Mobile">
-                                            <label for="mobile">Mobile</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-floating">
-                                            <input type="password" class="form-control" placeholder="Password"
-                                                name="password" id="message"></input>
-                                            <label for="message">Password</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <button class="btn btn-primary w-100 py-3" type="submit">Sign Up</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                                </form>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
